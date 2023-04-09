a83e40bb69b3807a1d220071c248fa8bd0c5afdc
hasanMohamed99 <hasanmohamed09@gmail.com>
2023-04-09 02:22:49 +0200

    apply your google api key

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -2,7 +2,7 @@ class MyStrings{
   static const String loginScreen = '/login-screen';
   static const String otpScreen = '/otp-screen';
   static const String mapScreen = '/map-screen';
-  static const String googleApiKey = 'AIzaSyBGU2IkihGELeWGQSzgz0n8yQUvulrmvBM';
+  static const String googleApiKey = '';
   static const String suggestionsBaseUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
   static const String placeLocationBaseUrl = 'https://maps.googleapis.com/maps/api/place/details/json';
   static const String directionsBaseUrl = 'https://maps.googleapis.com/maps/api/directions/json';
